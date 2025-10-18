dc194b13cda484be9dd3aa18301d3972fc04b6ac
petereshac45 <petereshac45@gmail.com>
2025-10-18 08:51:01 +0300

    new

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import HomeView
+from products.views import HomeView ,  AboutView , ContactView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view() , name='home'),
-     path('products/', include('products.urls')),
+  path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('products/', include(('products.urls', 'products'), namespace='products')),
     path('accounts/', include('accounts.urls')),
         path("orders/", include("orders.urls")),
            path("cart/", include("cart.urls")),
--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,10 @@
-from .views import ProductListView,HomeView ,ProductDetailView ,ProductBuyView , AboutView , ContactView
 from django.urls import path
+from django.urls import path
+from .views import ProductListView, ProductDetailView, ProductBuyView
+app_name = 'products'
 
-from products.views import ContactView
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('contact/', ContactView.as_view(), name='contact'),
-      path('', ProductListView.as_view(), name='product_list'),
-     path('products/buy/<int:pk>/', ProductBuyView.as_view(), name='product_buy'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('buy/<int:pk>/', ProductBuyView.as_view(), name='product_buy'),
 ]
