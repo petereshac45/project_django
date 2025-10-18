eb11b6d94419cd0082f981afecf1b645b6c3a992
petereshac45 <petereshac45@gmail.com>
2025-10-18 09:27:40 +0300

    gf

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -6,13 +6,15 @@ from products.views import HomeView, AboutView, ContactView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+   
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
     path('contact/', ContactView.as_view(), name='contact'),
-    path('products/', include('products.urls')),
+
+ 
+    path('products/', include(('products.urls', 'products'), namespace='products')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('cart/', include('cart.urls')),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
