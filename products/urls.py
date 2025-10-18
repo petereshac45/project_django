607a4e4c7ed83a1562a4dd020c977e7b49e9a33a
petereshac45 <petereshac45@gmail.com>
2025-10-18 07:58:33 +0300

    try

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,15 @@
 from .views import ProductListView,HomeView ,ProductDetailView ,ProductBuyView , AboutView , ContactView
 from django.urls import path
-
+app_name = "products"
 from products.views import ContactView
+
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('contact/', ContactView.as_view(), name='contact'),
-      path('', ProductListView.as_view(), name='product_list'),
-     path('products/buy/<int:pk>/', ProductBuyView.as_view(), name='product_buy'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    # /products/
+    path("", ProductListView.as_view(), name="product_list"),
+
+    # /products/123/
+    path("<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+
+    # /products/buy/123/
+    path("buy/<int:pk>/", ProductBuyView.as_view(), name="product_buy"),
 ]
